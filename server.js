b3bf8d951ce66c338554a04466bb88c02200cfed
Divyanshu6797 <divyanshu6797@gmail.com>
2024-05-27 14:46:16 +0530

    cors fix

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
     cors: {
-        origin: "*",
-        methods : ["GET", "POST"]
+        origin: "*:*",
+        methods : ["GET", "POST"],
+        headers : ["Access-Control-Allow-Origin"],
        
       }
 });
