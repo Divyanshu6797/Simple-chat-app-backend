687128d6a9e427ff4cb7cf449be2b169026011d6
Divyanshu6797 <divyanshu6797@gmail.com>
2024-05-27 13:42:56 +0530

    done

---
+++ server.js
@@ -0,0 +1,23 @@
+const app = require("express")();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+       
+      }
+});
+
+io.on("connection", (socket) => {
+  console.log("this is socket :", socket);
+  console.log("a user connected");
+  socket.on("chat", (payload) => {
+    console.log("this is payload :", payload);
+    io.emit("chat", payload);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("listening on port 5000");
+});
